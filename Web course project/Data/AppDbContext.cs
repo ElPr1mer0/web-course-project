52e5e8eed9618c85605804e493a7cc47039e9776
ElPr1mer0 <ilya.kudlach@yandex.ru>
2024-04-10 00:11:41 +0300

    add AppDbContext and users entity

---
+++ Web course project/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_course_project.Data.Entities;
+
+namespace Web_course_project.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ Web course project/Data/Entities/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace Web_course_project.Data.Entities
+{
+    public class Users
+    {
+        public int userId { get; set; }
+        public string name { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public int roleId { get; set; }
+    }
+}
